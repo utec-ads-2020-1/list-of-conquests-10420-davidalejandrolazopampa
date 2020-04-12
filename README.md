08f9e30141e1075df44807c2329fc29489b9aacf
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2020-04-12 02:41:55 -0500

    Iniciando

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trabajo
+Uva - 10420 "list of conquests"
+## Alumno
+David Lazo - 201810010
+```
+Iniciando
+```
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//David Lazo 201810010
+//UVA 10420
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    return 0;
+}
